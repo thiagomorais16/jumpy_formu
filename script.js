52b4c9c5bdc2e2862b4290d7c067d201d4bb11a6
Thiago Morais <118579263+thiagomorais16@users.noreply.github.com>
2023-01-17 16:41:08 -0300

    realise-5/2
    upgrade JS

--- script.js
+++ script.js
@@ -1,60 +1,80 @@
-const form = document.getElementById('form')
-const username = document.getElementById('username')
-const email = document.getElementById('email')
-const tel = document.getElementById('tel')
-const cpf = document.getElementById('cpf')
-const password = document.getElementById('password')
+const form   = document.getElementById('form');
+const campos = document.querySelectorAll('.required');
+const spam = document.querySelectorAll('.span-required');
 
-form.addEventListener ('submit', (e) => {
-    e.preventDefault()
+form.addEventListener('submit',(event) => {
+    event.preventDefault();
+    nameValidate();
+    emailValidate();
+    telValidate();
+    cpfValidate();
+    passwordValidate();
+});
 
-    checkInputs()
-})
 
-function checkInputs(){
-    const usernameValue = username.value
-    const emailValue = email.value
-    const telValue = tel.value
-    const cpfValue = cpf.value
-    const passwordValue = password.value
-
-    if(username === ''){
-        errorValidation(username, 'Preencha esse dado')
-    }else{
-        successValidation(username)
-    }
-    if(telValue ===''){
-    errorValidation(tel,'Preencha esse dado')
-    }else{
-    successValidation(tel)
-    }
-    if (cpfValue === '') {
-        errorValidation(cpf, 'Preencha esse dado')
-    } else if (cpfValue.length < 11) {
-        errorValidation(cpf, 'O cpf precisa ter + de 11 caracteres')
-    } else {
-        successValidation(cpf)
-    }
-    if (passwordValue === '') {
-        errorValidation(password, 'Preencha esse dado')
-    } else if (passwordValue.length < 8) {
-        errorValidation(password, 'A senha deve ter + de 8 caracteres')
-    } else {
-        successValidation(email)
-    }
-
+function setError(index){
+   campos[index].style.border = '3px solid #e63636';
+   spam[index].style.display = 'block';
 }
 
-function errorValidation(input, message){
-    const formControl = input.parentElement;
+function removeError(index) {
+    campos[index].style.border = '3px solid #048d2d';
+    spam[index].style.display = 'none';
+}
+
+function nameValidate(){
+    if(campos[0].value.length < 3)
+    {
+        setError(0);   
+ }
+    else 
+    {
+        removeError(0);
     
+    }
 }
+    function emailValidate(){
+        if(campos[1].value.length < 15)
+        {
+            setError(1);   
+     }
+        else 
+        {
+            removeError(1);
+        
+        }
 
-function successValidation (input) {
-    const formControl = input.parentElement ; 
-
-    innerText = message
-
-  alert ("Sucesso")
-}
-
+    }
+    function telValidate(){
+        if(campos[2].value.length < 11)
+        {
+            setError(2);   
+     }
+        else 
+        {
+            removeError(2);
+        
+        }
+    }
+    function cpfValidate(){
+        if(campos[3].value.length == 14)
+        {
+            removeError(3);
+     }
+        else 
+        {
+            setError(3);
+        
+        }
+    }
+    function passwordValidate(){
+        if(campos[4].value.length <= 8)
+        {
+            setError(4);   
+     }
+        else 
+        {
+            removeError(4);
+        
+        }
+    }
